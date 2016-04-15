1ae6eb70ccecf8cead3da360260567c6eb4fa771
web521 <wdjlwz@outlook.com>
2016-04-15 17:16:16 +0800

    20160415 commit new code

--- pub/ReadExcelinfo.py
+++ pub/ReadExcelinfo.py
@@ -12,6 +12,17 @@ sshport_list = []
 telnetport_list = []
 daynight_list = []
 light_list = []
+contrast_list = []
+saturation_list = []
+sharpnes_list = []
+exposureh5_list = []
+GainMode_list = []
+gainmax_list = []
+IrisType_list = []
+irislevel_list = []
+ShuterMode_list = []
+Shutermin_list = []
+AntiFlickerMode_list = []
 #处理
 try:
     sheet = opensheet.sheet_by_name("message")
@@ -36,7 +47,7 @@ for i in range(2,nrows): #行 循环
     telnetport_data = sheet.cell_value(i,6)
     telnetport_list.append(telnetport_data)
     #print (telnetport_list)
-
+'''
 try:
     table = opensheet.sheet_by_name("testcases")
 except:
@@ -50,12 +61,33 @@ for n in range(2,ncol2):
     #print daynight_data
 for n in range(3,ncol2):
     light_data=table.cell(2,n).value
-
     light_list.append(light_data)
+'''
 def getdata(value): 
     table = opensheet.sheet_by_name("testcases")
-    test=table.row_values(1)
-    data = test[3:-1]    
-    print data
+    test=table.row_values(value)
+    data = test[3:-1]
+    while ''  in data:
+        data1=data.remove('' ) 
+    return data
+    #print data
+    
+daynight_list = getdata(1)
+light_list = getdata(2)
+contrast_list = getdata(3)
+saturation_list = getdata(4)
+sharpnes_list = getdata(5)
+GainMode_list = getdata(6)
+gainmax_list = getdata(7)
+IrisType_list = getdata(8)
+irislevel_list = getdata(9)
+ShuterMode_list = getdata(10)
+Shutermin_list = getdata(11)
+AntiFlickerMode_list = getdata(12)
 if __name__ =='__main__':
-    getdata(1)
+    print daynight_list
+    print light_list
+    print contrast_list
+    print saturation_list
+    print AntiFlickerMode_list
+    print Shutermin_list 
--- verifylib/Verify_ImageSetting.py
+++ verifylib/Verify_ImageSetting.py
@@ -5,6 +5,7 @@ import paramiko
 import time
 import datetime
 from pub import ReadExcelinfo
+from __builtin__ import str
 username = ReadExcelinfo.user_data
 passwd = ReadExcelinfo.password_data
 ip = ReadExcelinfo.ip_data
@@ -27,21 +28,49 @@ class C_Imagesetting_ver():
     def ircut_ver(self):
         cmd = ["cat /usr/config/ipccfg.conf|awk -F DayNightMode '{print $5}'|awk -F , '{print $1"" }'|awk -F : '{print $2}'"]
         val=self.verifyipc(cmd)
+        val = str(val)
+        return val
         print val
-    def lightchange_ver(self):
+    def lightchange_ver(self):  ##读取配置文件亮度
         cmd = ["cat /usr/config/ipccfg.conf|awk -F Brightness '{print $4}'|awk -F , '{print $1}'|awk -F : '{print $2}'"]
         val=self.verifyipc(cmd)
         val=str(val)
-        y=type(val)
+        #y=type(val)
         #print "lightchange_ver+y:"
         #print y
         return val
-
+    def contrast_ver(self):  ##读取配置文件对比度
+        cmd = [ "cat /usr/config/ipccfg.conf|awk -F Contrast '{print $3}'|awk -F , '{print $1}'|awk -F : '{print $2}'"]
+        val=self.verifyipc(cmd)
+        val=str(val)
+        #y=type(val)
+        #print "lightchange_ver+y:"
+        #print y
+        return val
+    def saturation_ver(self):  ##读取配置文件饱和度
+        cmd = ["cat /usr/config/ipccfg.conf|awk -F Saturation '{print $3}'|awk -F , '{print $1}'|awk -F : '{print $2}'"]
+        val=self.verifyipc(cmd)
+        val=str(val)
+        #y=type(val)
+        #print "lightchange_ver+y:"
+        #print y
+        return val
+    def sharpness_ver(self):  ##读取配置文件锐度
+        cmd = ["cat /usr/config/ipccfg.conf|awk -F Sharpness '{print $3}'|awk -F , '{print $1}'|awk -F : '{print $2}'"]
+        val=self.verifyipc(cmd)
+        val=str(val)
+        #y=type(val)
+        #print "lightchange_ver+y:"
+        #print y
+        return val
+    
 if __name__=='__main__':
     test=C_Imagesetting_ver()
     xxx=test.lightchange_ver()
+    yyy = test.ircut_ver()
     print 'xxx:'
     print xxx
+    print yyy
 
 
 
